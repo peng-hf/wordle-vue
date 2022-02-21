1eb36225265ce0178d06d1d0038e12b48658b664
Philippe <ilyfing@gmail.com>
2022-02-21 00:16:36 -0600

    setup eslint

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,27 @@ import { ref } from 'vue'
 
 const darkMode = ref(true)
 
-function toggleDarkMode() {
+function toggleDarkMode () {
   darkMode.value = !darkMode.value
   document.body.classList.toggle('dark')
 }
 
-
 </script>
 
 <template>
-<div class="h-screen w-screen bg-light-500 dark:bg-dark-500">
-  <!-- Header -->
-  <div class="h-60px flex justify-between items-center text-3xl font-semi-bold dark:text-light-500">
-    <div class="w-50px text-center"></div>
-    <div>WORDLE</div>
-    <i :class="`w-50px p-2 text-center cursor-pointer eva eva-${darkMode ? 'sun-outline' : 'moon-outline'}`" @click="toggleDarkMode"></i>
+  <div class="h-screen w-screen bg-light-500 dark:bg-dark-500">
+    <!-- Header -->
+    <div class="header">
+      <div class="w-50px text-center" />
+      <div>WORDLE</div>
+      <i
+        :class="`w-50px p-2 text-center cursor-pointer eva eva-${darkMode ? 'sun-outline' : 'moon-outline'}`"
+        @click="toggleDarkMode"
+      />
+    </div>
   </div>
-  <hr class="border-gray-300 dark:border-gray-200" />
-</div>
 </template>
 
 <style>
-
+.header {}
 </style>
