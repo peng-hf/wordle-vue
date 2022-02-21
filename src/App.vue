7ca09cc38f87e69fd186e0b4f4b5e0a8d97525ff
Philippe <ilyfing@gmail.com>
2022-02-20 19:01:16 -0600

    Implemented initial header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+console.log('hello')
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div class="h-screen w-screen bg-light-500 dark:bg-dark-500">
+  <!-- Header -->
+  <div class="h-60px flex justify-center items-center text-3xl font-semi-bold dark:text-white">WORDLE</div>
+  <hr class="dark:border-gray-500" />
+
+
+
+</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
