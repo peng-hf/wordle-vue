d485309cb17c8d529d21bd8964e1e2201116c194
Philippe <ilyfing@gmail.com>
2022-12-04 18:15:31 -0600

    add key animation

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,51 +1,70 @@
-<script setup>
-import { ref } from 'vue'
+<script>
 import { getCellBgRevealedColor } from '@/util'
-
-const props = defineProps({
-  matrix: {
-    type: Array,
-    required: true
-  }
-})
-
-const emit = defineEmits(['pressKey', 'restart'])
-
-document.addEventListener('keydown', pressKey)
 
 const KEYS = [
   [...'qwertyuiop'.split('')],
   [...'asdfghjkl'.split('')],
   ['Enter', ...'zxcvbnm'.split(''), 'Backspace']
 ]
-
-const keys = ref(KEYS)
-
 const ALLOWED_KEYS = KEYS.reduce((acc, row) => acc.concat(row), [])
 
-function pressKey(evt) {
-  const key = evt instanceof KeyboardEvent ? evt.key : evt
-  if (ALLOWED_KEYS.includes(key)) emit('pressKey', key)
-  if (evt.shiftKey && key === 'R') emit('restart')
-}
+export default {
+  props: {
+    matrix: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ['pressKey', 'restart'],
+  data: () => ({
+    KEYS
+  }),
+  mounted() {
+    const mapKeyToElt = ALLOWED_KEYS.reduce((acc, key) => {
+      const elt = document.querySelector(`#key-${key}`)
+      acc[key] = elt
+      return acc
+    }, {})
 
-function getKeyColorClasses(key) {
-  for (let i = props.matrix.length - 1; i >= 0; i--) {
-    const row = props.matrix[i]
-    const foundCell = row.find(c => c.letter === key)
-    if (foundCell?.reveal) {
-      return `${getCellBgRevealedColor(foundCell.state)} text-white`
+    document.addEventListener('keydown', evt => {
+      if (ALLOWED_KEYS.includes(evt.key)) {
+        this.$emit('pressKey', evt.key)
+        const keyElt = mapKeyToElt[evt.key]
+        keyElt.classList.add('opacity-70')
+      }
+      if (evt.shiftKey && evt.key === 'R') this.$emit('restart')
+    })
+
+    document.addEventListener('keyup', evt => {
+      const keyElt = mapKeyToElt[evt.key]
+      console.log(keyElt)
+      if (keyElt) keyElt.classList.remove('opacity-70')
+    })
+  },
+  methods: {
+    onClickKey(key) {
+      if (ALLOWED_KEYS.includes(key)) this.$emit('pressKey', key)
+    },
+    getKeyColorClasses(key) {
+      for (let i = this.matrix.length - 1; i >= 0; i--) {
+        const row = this.matrix[i]
+        const foundCell = row.find(c => c.letter === key)
+        if (foundCell?.reveal) {
+          return `${getCellBgRevealedColor(foundCell.state)} text-white`
+        }
+      }
+      return 'bg-light-800 dark:(bg-dark-50 text-white) hover:opacity-70 active:opacity-40'
     }
   }
-  return 'bg-light-800 dark:(bg-dark-50 text-white) hover:opacity-70 active:opacity-40'
 }
 </script>
 
 <template>
   <div>
-    <div v-for="(_, rowIdx) in keys" :key="rowIdx" class="flex justify-center">
+    <div v-for="(_, rowIdx) in KEYS" :key="rowIdx" class="flex justify-center">
       <div
-        v-for="key in keys[rowIdx]"
+        v-for="key in KEYS[rowIdx]"
+        :id="`key-${key}`"
         :key="key"
         :class="[
           'h-60px w-40px m-3px flex justify-center items-center gap-3px uppercase rounded-sm font-bold text-lg cursor-pointer',
@@ -53,7 +72,7 @@ function getKeyColorClasses(key) {
           { '!w-80px !text-3xl': key === 'Backspace' },
           getKeyColorClasses(key)
         ]"
-        @click="pressKey(key)"
+        @click="onClickKey(key)"
       >
         <i v-if="key === 'Backspace'" class="eva eva-backspace-outline" />
         <template v-else>{{ key }}</template>
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -9,11 +9,11 @@ defineProps({
 
 
 <template>
-  <div class="absolute w-full h-full flex flex-col pt-1/5 items-center">
+  <div class="absolute w-full flex flex-col pt-1/5 items-center">
     <div
       v-for="(msg, idx) in messages"
       :key="idx"
-      class="py-12px px-20px rounded-lg mt-10px bg-light-400 text-black text-center shadow-lg z-1"
+      class="py-12px px-20px rounded-md mt-10px bg-light-400 text-black text-center shadow-lg z-1"
     >
       <span v-html="msg.text" />
     </div>
